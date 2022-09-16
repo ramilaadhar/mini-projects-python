8a4f1653846ac3424997ab1899608222564f5b22
unknown <rami.laadhar@gmail.com>
2022-09-16 13:14:04 +0100

    Edit scissor_stone

--- scissor_stone.py
+++ scissor_stone.py
@@ -1,23 +1,40 @@
 import random
-choix=['C','P','PI']
-cpu=random.choice(choix)
-joueur=str(input("C:ciseau , P:papier , PI:pierre?")).capitalize()
-if joueur==cpu:
+choix=['C','P','Pi']
+score_cpu=0
+score_joueur=0
+
+while True:
+   cpu = random.choice(choix)
+   joueur = str(input("C:ciseau , P:papier , PI:pierre? Pour quiter taper Fin ")).capitalize()
+   if joueur==cpu:
     print("égalite!")
-elif joueur=='PI':
+   elif joueur=='Pi':
         if cpu=='P':
             print("vous perdez, le papier couvre la pierre")
-        elif cpu=='C':
+            score_cpu+=1
+   elif cpu=='C':
             print("vous gagner, la pierre casse les ciseaux")
+            score_joueur+=1
 
-elif joueur=='P':
+   elif joueur=='P':
         if cpu=='C':
             print("vous perdez, les ciseaux coupe le papier")
-        elif cpu=='PI':
+            score_cpu+=1
+        elif cpu=='Pi':
             print("vous gagner, le papier couvre la pierre")
+            score_joueur+=1
 
-elif joueur=='C':
+   elif joueur=='C':
         if cpu=='P':
             print("vous gangez, les ciseaux coupe le papier")
-        elif cpu=='PI':
+            score_joueur+=1
+        elif cpu=='Pi':
             print("vous perder, la pierre casse les ciseaux")
+            score_cpu+=1
+   elif joueur=='Fin':
+    print("resultat:")
+    print(f"cpu= {score_cpu}")
+    print(f"joueur= {score_joueur}")
+    break
+   else:
+    print("je n'est pas comprit: verifier l'orthographe")
